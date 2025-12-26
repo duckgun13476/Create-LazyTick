2cc5bc5e783c7b0877b8c65aab30696bfe64c2c9
Pink_Cats <1907284584@qq.com>
2025-12-26 13:56:52 +1100

    HotFix (2/?) Add Responsive feature to clientData sync.

--- src/main/java/net/pinkcats/createlazytick/mixin/Create/Client/SmartBlockEntityGoggleMixin.java
+++ src/main/java/net/pinkcats/createlazytick/mixin/Create/Client/SmartBlockEntityGoggleMixin.java
@@ -8,6 +8,8 @@ import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
+import net.pinkcats.createlazytick.Channel.CLTChannel;
+import net.pinkcats.createlazytick.Channel.ClockSyncPacket;
 import net.pinkcats.createlazytick.Config;
 import net.pinkcats.createlazytick.bridge.Create.ISmartBlockEntityControl;
 import net.pinkcats.createlazytick.item.LazyTickClockItem;
@@ -19,6 +21,12 @@ import java.util.List;
 @Mixin(value = SmartBlockEntity.class, remap = false)
 public class SmartBlockEntityGoggleMixin implements IHaveHoveringInformation {
 
+    @Unique
+    private static final int createLazyTick$Frequent = 60;
+
+    @Unique
+    private int createLazyTick$tick = 0;
+
     @Override
     public boolean addToTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
         Minecraft mc = Minecraft.getInstance();
@@ -29,7 +37,24 @@ public class SmartBlockEntityGoggleMixin implements IHaveHoveringInformation {
 
         if (this instanceof ISmartBlockEntityControl control) {
 
-            System.out.println("  " + control.createLazyTick$ControlState());
+            // Handle packet
+            createLazyTick$tick++;
+            if (createLazyTick$tick >= createLazyTick$Frequent) {
+                createLazyTick$tick = 0;
+                CLTChannel.sendToServer(new ClockSyncPacket(
+                        mc.player.getModelName(),
+                        control.CLT$getDimension().hashCode(),
+                        control.CLT$getPos()
+
+
+                ));
+
+                System.out.println("  " + ((ISmartBlockEntityControl) this).CLT$getDimension());
+            }
+
+
+
+
             // [UI修复] 防止图标遮挡
             if (tooltip.isEmpty()) {
                 // 如果是第一行（置物台），加4个空格缩进，给左侧图标留位置
