53e0d68a178b1f1d573c8db129982b1ede942560
Fugit-5414 <1322594375@qq.com>
2026-01-24 01:44:11 +0800

    Feat: Apply Scroll UI Behaviour to machines and fix Chute logic
    1. Integrated Scroll UI Behaviour into Mechanical Arm, Chute, Mechanical Crafter (Redstone Detection), Item Drain, and Depot.
    
    2. Chute: Removed the 'mistake' logic to resolve timer desynchronization bugs.

--- src/main/java/net/pinkcats/createlazytick/mixin/OptElement/crafter/CrafterRedstoneLazyTickMixin.java
+++ src/main/java/net/pinkcats/createlazytick/mixin/OptElement/crafter/CrafterRedstoneLazyTickMixin.java
@@ -2,6 +2,7 @@ package net.pinkcats.createlazytick.mixin.OptElement.crafter;
 
 import com.simibubi.create.content.kinetics.crafter.MechanicalCrafterBlockEntity;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.pinkcats.createlazytick.Config;
 import net.pinkcats.createlazytick.bridge.Create.ISmartBlockEntityControl;
 import net.pinkcats.createlazytick.helper.LazyTickLogic;
+import net.pinkcats.createlazytick.helper.LazyTickScrollBehaviour;
 import net.pinkcats.createlazytick.helper.NetworkSyncHelper;
 import net.pinkcats.createlazytick.helper.ScheduleTicker;
 import net.pinkcats.createlazytick.helper.extraDataTool.CrafterExtraDataTool;
@@ -105,6 +107,11 @@ public abstract class CrafterRedstoneLazyTickMixin extends SmartBlockEntity impl
                 LazyTickLogic.setIntervalSafe(this, newDelayTick);
             }
         }
+    }
+
+    @Inject(method = "addBehaviours", at = @At("RETURN"), remap = false)
+    private void lazytick$addScrollBehaviour(List<BlockEntityBehaviour> behaviours, CallbackInfo ci) {
+        LazyTickScrollBehaviour.addTo(this, behaviours);
     }
 
     @Inject(method = "tick", at = @At("HEAD"), remap = false)
