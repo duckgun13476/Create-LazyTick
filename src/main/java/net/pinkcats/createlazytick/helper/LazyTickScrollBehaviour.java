330ae48e477861d8f85a55e09a9468373daa4b5d
Fugit-5414 <1322594375@qq.com>
2026-02-05 12:51:28 +0800

    Refactor: Switch to UUID auth and separate display logic
    1.Fixed security issues where inconsistent names caused access denial or allowed impersonation. Permission checks are now strictly based on UUIDs.
    
    2.Separated concerns: UUIDs are used for logic and authentication, while cached names are used for UI rendering and command suggestions.

--- src/main/java/net/pinkcats/createlazytick/helper/LazyTickScrollBehaviour.java
+++ src/main/java/net/pinkcats/createlazytick/helper/LazyTickScrollBehaviour.java
@@ -131,6 +131,7 @@ public class LazyTickScrollBehaviour extends ScrollValueBehaviour {
 
         if (blockEntity instanceof ISmartBlockEntityControl control) {
             control.createLazyTick$setOwnerName(player.getName().getString());
+            control.createLazyTick$setOwnerUUID(player.getUUID());
         }
 
         int newValue = settings.value();
