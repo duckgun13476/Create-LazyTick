0bebd48fdb8405fc128b19c68b42547af6e4bcd6
Pink_Cats <1907284584@qq.com>
2026-02-12 20:14:07 +1100

    1.Fix fluid system.

---
+++ src/main/java/net/pinkcats/createlazytick/mixin/OptElement/fluid/FluidNetworkTransferSpeedMixin.java
@@ -0,0 +1,35 @@
+package net.pinkcats.createlazytick.mixin.OptElement.fluid;
+
+
+import com.simibubi.create.content.fluids.FluidNetwork;
+import me.lucko.spark.lib.asm.Opcodes;
+import net.pinkcats.createlazytick.config.ServerConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+
+@Mixin(value = FluidNetwork.class, remap = false)
+    public abstract class FluidNetworkTransferSpeedMixin {
+
+        @Shadow
+        int transferSpeed;
+
+        @Redirect(
+                method = "tick",
+                at = @At(
+                        value = "FIELD",
+                        target = "Lcom/simibubi/create/content/fluids/FluidNetwork;transferSpeed:I",
+                        opcode = Opcodes.PUTFIELD
+                ),
+                require = 0
+        )
+        private void createlazytick$scaleTransferSpeed(FluidNetwork self, int original) {
+            float mult = ServerConfig.getFluidDelayMax();
+            this.transferSpeed = (int) Math.max(1, original * mult);
+        }
+    }
+
+
+
