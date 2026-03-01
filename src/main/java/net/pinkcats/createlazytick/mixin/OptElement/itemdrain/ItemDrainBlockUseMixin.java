1b5ae283ba5cf908785b0d1f6f1cdda615418357
Fugit-5414 <1322594375@qq.com>
2026-03-01 15:21:49 +0800

    refactor: migrate mixin package to NeoForge 1.21.1 (1/4)(Low Risk)

--- src/main/java/net/pinkcats/createlazytick/mixin/OptElement/itemdrain/ItemDrainBlockUseMixin.java
+++ src/main/java/net/pinkcats/createlazytick/mixin/OptElement/itemdrain/ItemDrainBlockUseMixin.java
@@ -3,8 +3,9 @@ package net.pinkcats.createlazytick.mixin.OptElement.itemdrain;
 import com.simibubi.create.content.fluids.drain.ItemDrainBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -17,12 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ItemDrainBlock.class)
 public class ItemDrainBlockUseMixin {
 
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void createLazyTick$openScrollerUi(BlockState state, Level level, BlockPos pos, Player player,
-                                                InteractionHand hand, BlockHitResult hit,
-                                                CallbackInfoReturnable<InteractionResult> cir) {
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    private void createLazyTick$openScrollerUi(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player,
+                                               InteractionHand hand, BlockHitResult hit,
+                                               CallbackInfoReturnable<ItemInteractionResult> cir) {
         if (LazyTickScrollerOpenHelper.tryOpen(level, pos, player, hand, hit)) {
-            cir.setReturnValue(InteractionResult.CONSUME);
+            cir.setReturnValue(ItemInteractionResult.CONSUME);
         }
     }
 }
