39d3d562c9c430636e879d6f6ee58bd3662bf942
Fugit-5414 <1322594375@qq.com>
2026-03-01 15:21:49 +0800

    refactor: migrate mixin package to NeoForge 1.21.1 (1/4)(Low Risk)

--- src/main/java/net/pinkcats/createlazytick/mixin/OptElement/depot/DepotBlockUseMixin.java
+++ src/main/java/net/pinkcats/createlazytick/mixin/OptElement/depot/DepotBlockUseMixin.java
@@ -3,8 +3,9 @@ package net.pinkcats.createlazytick.mixin.OptElement.depot;
 import com.simibubi.create.content.logistics.depot.DepotBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -17,12 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DepotBlock.class)
 public class DepotBlockUseMixin {
 
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void createLazyTick$openTestUiOnLowerHalf(BlockState state, Level level, BlockPos pos, Player player,
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    private void createLazyTick$openTestUiOnLowerHalf(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player,
                                                       InteractionHand hand, BlockHitResult hit,
-                                                      CallbackInfoReturnable<InteractionResult> cir) {
+                                                      CallbackInfoReturnable<ItemInteractionResult> cir) {
         if (LazyTickScrollerOpenHelper.tryOpen(level, pos, player, hand, hit)) {
-            cir.setReturnValue(InteractionResult.CONSUME);
+            cir.setReturnValue(ItemInteractionResult.CONSUME);
         }
     }
 }
--- src/main/java/net/pinkcats/createlazytick/mixin/OptElement/saw/SawBlockUseMixin.java
+++ src/main/java/net/pinkcats/createlazytick/mixin/OptElement/saw/SawBlockUseMixin.java
@@ -3,8 +3,9 @@ package net.pinkcats.createlazytick.mixin.OptElement.saw;
 import com.simibubi.create.content.kinetics.saw.SawBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
@@ -17,12 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SawBlock.class)
 public class SawBlockUseMixin {
 
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void createLazyTick$openScrollerUi(BlockState state, Level level, BlockPos pos, Player player,
-                                                InteractionHand hand, BlockHitResult hit,
-                                                CallbackInfoReturnable<InteractionResult> cir) {
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    private void createLazyTick$openScrollerUi(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player,
+                                               InteractionHand hand, BlockHitResult hit,
+                                               CallbackInfoReturnable<ItemInteractionResult> cir) {
         if (LazyTickScrollerOpenHelper.tryOpen(level, pos, player, hand, hit)) {
-            cir.setReturnValue(InteractionResult.CONSUME);
+            cir.setReturnValue(ItemInteractionResult.CONSUME);
         }
     }
 }
