f260a473dd970cd3e65b93bce7231a601e009d44
Fugit-5414 <1322594375@qq.com>
2026-03-03 16:37:02 +0800

    refactor: migrate menu package in NutUI to NeoForge 1.21.1

--- src/main/java/net/pinkcats/NutUI/menu/ClientScreen.java
+++ src/main/java/net/pinkcats/NutUI/menu/ClientScreen.java
@@ -1,23 +1,20 @@
 package net.pinkcats.NutUI.menu;
 
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.pinkcats.NutUI.menu.extensions.NutMenuScreenRouter;
 
 import static net.pinkcats.NutUI.menu.NutKineticMenu.ItemMenuRegiste;
 import static net.pinkcats.createlazytick.CreateLazyTick.MODID;
 
-@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
 public class ClientScreen {
 
-
     @SubscribeEvent
-    public static void registerScreen(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> MenuScreens.register(ItemMenuRegiste.get(), NutMenuScreenRouter::create));
+    public static void registerScreen(RegisterMenuScreensEvent event) {
+        event.register(ItemMenuRegiste.get(), NutMenuScreenRouter::create);
     }
-
 
 }
