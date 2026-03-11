e1a622c18830eb47ef6d59fe48c718a08f2c18de
Pink_Cats <1907284584@qq.com>
2026-03-11 19:55:19 +0800

    1.remove duplicate register.

--- src/main/java/net/pinkcats/createlazytick/Register/ClientInit.java
+++ src/main/java/net/pinkcats/createlazytick/Register/ClientInit.java
@@ -1,27 +1,14 @@
 package net.pinkcats.createlazytick.Register;
 
-import com.simibubi.create.AllCreativeModeTabs;
 import net.createmod.catnip.config.ui.BaseConfigScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.pinkcats.createlazytick.CreateLazyTick;
 import net.pinkcats.createlazytick.config.ClientConfig;
 import net.pinkcats.createlazytick.config.ServerConfig;
 
-@EventBusSubscriber(modid = CreateLazyTick.MODID, value = Dist.CLIENT)
 public class ClientInit {
-
-    @SubscribeEvent
-    public static void onBuildCreativeTab(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTab() == AllCreativeModeTabs.BASE_CREATIVE_TAB.get()) {
-            event.accept(LazyTickItem.CLOCK.get());
-        }
-    }
 
     public static void initClient(ModContainer modContainer) {
         modContainer.registerExtensionPoint(
